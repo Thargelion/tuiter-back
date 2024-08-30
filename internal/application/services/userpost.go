1a2e04ef3f840d67471d09e598094b8e39a08ede
Maximiliano De Pietro <elwindwalker@gmail.com>
2024-08-30 20:50:02 -0300

    chore: :rotating_light: fix error wrapings

--- internal/application/services/userpost.go
+++ internal/application/services/userpost.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"fmt"
 
 	"tuiter.com/api/internal/domain/tuit"
 	"tuiter.com/api/internal/domain/userpost"
@@ -20,25 +21,43 @@ type UserPostService struct {
 }
 
 func (u *UserPostService) Paginate(ctx context.Context, userID int, page int) ([]*userpost.UserPost, error) {
-	return u.userPostRepository.ListByPage(ctx, page, userID)
+	userTuitPage, err := u.userPostRepository.ListByPage(ctx, page, userID)
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving user posts page: %w", err)
+	}
+
+	return userTuitPage, nil
 }
 
 func (u *UserPostService) AddLike(ctx context.Context, userID int, tuitID int) (*userpost.UserPost, error) {
 	err := u.tuitRepository.AddLike(ctx, userID, tuitID)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error adding like: %w", err)
 	}
 
-	return u.userPostRepository.GetByID(ctx, userID, tuitID)
+	userTuit, err := u.userPostRepository.GetByID(ctx, userID, tuitID)
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving user post: %w", err)
+	}
+
+	return userTuit, nil
 }
 
 func (u *UserPostService) RemoveLike(ctx context.Context, userID int, tuitID int) (*userpost.UserPost, error) {
 	err := u.tuitRepository.RemoveLike(ctx, userID, tuitID)
 
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("error removing like: %w", err)
 	}
 
-	return u.userPostRepository.GetByID(ctx, userID, tuitID)
+	userTuit, err := u.userPostRepository.GetByID(ctx, userID, tuitID)
+
+	if err != nil {
+		return nil, fmt.Errorf("error retrieving user post: %w", err)
+	}
+
+	return userTuit, nil
 }
