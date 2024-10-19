066420b561e41e2d7484ce324e1abbb5c2d8e586
Maximiliano De Pietro <elwindwalker@gmail.com>
2024-10-19 16:05:26 -0300

    :shield: allow only with correct api secret

--- internal/application/handlers/middleware.go
+++ internal/application/handlers/middleware.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/google/uuid"
 	"golang.org/x/net/context"
@@ -19,5 +20,18 @@ func RequestTagger(next http.Handler) http.Handler {
 		ctx := context.WithValue(r.Context(), reqKey, identifier)
 
 		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func ApiValidation(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expected := os.Getenv("API_KEY")
+		if r.Header.Get("Api-Secret") != expected {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
 }
